a44f06caa2a3180c152f3785bea9e412b8a16fe6
maozinha23 <20055270+maozinha23@users.noreply.github.com>
2025-07-04 17:00:52 -0300

    Envio inicial

---
+++ 00-run_once-install.sh
@@ -0,0 +1,258 @@
+#!/bin/sh
+
+# Installation guide (Português)
+# https://wiki.archlinux.org/title/Installation_guide_(Portugu%C3%AAs)
+
+# Funções auxiliares {{{
+# Verifica a conectividade com a internet tentando pingar um servidor confiável
+is_connected() {
+  host='8.8.8.8'  # IP do DNS público do Google
+  count=2         # Número de tentativas de ping
+  timeout=5       # Tempo limite por tentativa em segundos
+
+  ping -c "${count}" -W "${timeout}" "${host}" > /dev/null 2>&1
+}
+# }}}
+
+# 1 - Pré-instalação {{{
+
+# 1.1 - Obter uma imagem de instalação {{{
+# }}}
+
+# 1.2 - Verificar a assinatura {{{
+# }}}
+
+#1.3 - Preparar uma mídia de instalação {{{
+# }}}
+
+# 1.4 - Inicializar o ambiente live {{{
+# }}}
+
+# 1.5 - Definir o layout e fonte do teclado do console {{{
+loadkeys br-abnt2
+setfont ter-120n
+# }}}
+
+# 1.6 - Definir o idioma do ambiente live {{{
+# }}}
+
+# 1.7 - Verificar o modo de inicialização {{{
+#cat /sys/firmware/efi/fw_platform_size
+# }}}
+
+# 1.8 - Conectar à internet {{{
+# Verifica se foi possível conectar à internet
+# Se não foi possível conectar por uma interface ethernet, tenta através de uma
+# interface wireless
+clear
+printf "Verificando a conexão com a internet ...\n"
+
+if ! is_connected; then
+  printf "Não foi possível conectar à internet.\n"
+
+  iwctl device list
+
+  printf "Escolha o dispositivo: "
+  read -r wifi_device
+
+  if iwctl device "${wifi_device}" set-property Powered on; then
+    iwctl station "${wifi_device}" scan
+    iwctl station "${wifi_device}" get-networks
+
+    printf "Escolha a rede sem fio: "
+    read -r wifi_ssid
+
+    printf "Digite a senha: "
+    stty -echo
+    read -r wifi_passphrase
+    stty echo
+    printf "\n"
+
+    printf "Tentando conectar-se a rede %s ...\n" "${wifi_ssid}"
+    iwctl --passphrase "${wifi_passphrase}" station "${wifi_device}" \
+      connect "${wifi_ssid}"
+  else
+    printf "Não foi possível ligar o dispositivo %s\n" "${wifi_device}"
+  fi
+fi
+
+# Finaliza o script se não foi possível conectar à internet
+if ! is_connected; then
+  printf "Não foi possível conectar à internet.\nO script de instalação será \
+encerrado."
+  exit 1
+fi
+# }}}
+
+# 1.9 - Atualizar o relógio do sistema {{{
+timedatectl set-timezone America/Sao_Paulo
+# }}}
+
+# 1.10 - Partição dos discos {{{
+# GUID para GPT:
+# EFI              C12A7328-F81F-11D2-BA4B-00A0C93EC93B
+# Linux Filesystem 0FC63DAF-8483-4772-8E79-3D69D8477DE4
+# Linux Swap       0657FD6D-A4AB-43C4-84E5-0933C84B4F4F
+#sdX1	EFI System
+#sdX2	Linux filesystem
+printf "O script utilizará o seguinte esquema de partições:\n\
+label: gpt\n\
+device: disco_escolhido\n\
+disco_escolhido1: EFI System       - tamanho: 100MiB\n\
+disco_escolhido2: Linux filesystem - tamanho: resto do disco\n"
+
+lsblk
+printf "Escolha o disco para instalação: "
+read -r disk_selected
+
+printf "O sistema será instalado no dispositivo %s.\n\
+TODOS OS DADOS DO DISCO SERÃO PERDIDOS!\n\
+Deseja continuar? Digite 's' para confirmar: " "${disk_selected}"
+read -r disk_confirmation
+
+if [ ! "${disk_confirmation}" = 's' ]; then
+  printf "O script de instalação será encerrado.\n"
+  exit 1
+fi
+
+cat << EOF | sfdisk /dev/"${disk_selected}"
+label: gpt
+device: /dev/${disk_selected}
+unit: sectors
+first-lba: 2048
+sector-size: 512
+
+/dev/${disk_selected}1 : start= , size=100M, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B
+/dev/${disk_selected}2 : start= , size= ,    type=0FC63DAF-8483-4772-8E79-3D69D8477DE4
+EOF
+# }}}
+
+# 1.11 - Formatar as partições {{{
+# -F: FAT SIZE
+mkfs.fat -F 32 /dev/"${disk_selected}"1
+mkfs.ext4 /dev/"${disk_selected}"2
+#mkswap /dev/partição_swap
+# }}}
+
+# 1.12 - Montar os sistemas de arquivos {{{
+mount /dev/"${disk_selected}"2 /mnt
+mount --mkdir /dev/"${disk_selected}"1 /mnt/boot
+#swapon /dev/partição_swap
+#}}}
+
+# }}}
+
+# 2 - Instalação {{{
+
+# 2.1 - Selecionar os espelhos {{{
+# /etc/pacman.d/mirrorlist
+# }}}
+
+# 2.2 - Instalar os pacotes essenciais {{{
+# Verifica se o sistema possui um CPU AMD ou Intel para instalar o microcode
+[ $(lscpu | grep --ignore-case --count 'amd') -gt 0 ] \
+  && cpu_microcode=amd-ucode \
+  || cpu_microcode=intel-ucode
+
+# amd-ucode : Microcode update image for AMD CPUs
+# base : Minimal package set to define a basic Arch Linux installation
+# base-devel : Basic tools to build Arch Linux packages
+# dosfstools : DOS filesystem utilities
+# e2fsprogs : Ext2/3/4 filesystem utilities
+# efibootmgr : Linux user-space application to modify the EFI Boot Manage
+# grub : GNU GRand Unified Bootloader (2)
+# intel-ucode : Microcode update files for Intel CPUs
+# linux :	The Linux kernel and modules
+# linux-firmware : Firmware files for Linux - Default set
+# man-db : A utility for reading man pages
+# man-pages : Linux man pages
+# neovim : Fork of Vim aiming to improve user experience, plugins, and GUIs
+# networkmanager : Network connection manager and user applications
+# ntfs-3g : NTFS filesystem driver and utilities
+# zsh : A very advanced and programmable command interpreter (shell) for UNIX
+package_list="base base-devel ${cpu_microcode} dosfstools e2fsprogs efibootmgr \
+  grub linux linux-firmware man-db man-pages neovim networkmanager ntfs-3g zsh"
+
+pacstrap -K /mnt "${package_list}"
+# }}}
+
+# }}}
+
+# 3 - Configurar o sistema {{{
+
+# 3.1 - Fstab {{{
+genfstab -U /mnt >> /mnt/etc/fstab
+# }}}
+
+# 3.2 - Chroot {{{
+arch-chroot /mnt /bin/sh -c '
+# }}}
+
+# 3.3 - Time {{{
+ln --symbolic --force /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+hwclock --systohc
+# }}}
+
+# 3.4 - Localização {{{
+# Remove o comentário de pt_BR.UTF-8 UTF-8 e gera os locales
+sed --in-place "s/#pt_BR.UTF-8/pt_BR.UTF-8/" /etc/locale.gen
+locale-gen
+
+# Cria o arquivo locale.conf e define a variável LANG adequadamente
+echo "LANG=pt_BR.UTF-8" > /etc/locale.conf
+
+# Armazena as definições do layout do teclado do console em vconsole.conf(5)
+echo "KEYMAP=br-abnt2" > /etc/vconsole.conf
+# }}}
+
+# 3.5 - Configuração de rede {{{
+# Cria o arquivo hostname:
+read -p "Digite seu hostname: " HOSTNAME
+echo "${HOSTNAME}" > /etc/hostname
+
+# /etc/hosts
+cat << EOF > /etc/hosts
+# The following lines are desirable for IPv4 capable hosts
+127.0.0.1 localhost
+127.0.1.1 ${HOSTNAME}.example.org ${HOSTNAME}
+
+# The following lines are desirable for IPv6 capable hosts
+::1 localhost ip6-localhost ip6-loopback
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+EOF
+
+# Ativa o serviço do NetworkManager na inicialização do sistema
+systemctl enable NetworkManager
+# }}}
+
+# 3.6 - Initramfs {{{
+# }}}
+
+# 3.7 - Senha do root {{{
+read -p "Digite seu nome de usuário: " user
+useradd --create-home --groups wheel --shell /bin/zsh "${user}"
+passwd "${user}"
+
+# Permite que usuários do grupo wheel executem qualquer comando
+#visudo
+sed --in-place "s/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/" /etc/sudoers
+
+# Desabilita o login do usuário root
+passwd --lock root
+# }}}
+
+# 3.8 - Gerenciador de boot {{{
+grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
+grub-mkconfig --output=/boot/grub/grub.cfg
+# }}}
+
+# }}}
+
+# 4 - Reiniciar {{{
+
+exit
+'
+umount -R /mnt
+reboot
+# }}}
