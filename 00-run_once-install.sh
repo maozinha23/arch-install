1e896a2baa5c23cf4295b2bc79b6725a033731cd
maozinha23 <20055270+maozinha23@users.noreply.github.com>
2025-08-13 19:17:42 -0300

    Alterada a formatação

--- 00-run_once-install.sh
+++ 00-run_once-install.sh
@@ -3,7 +3,7 @@
 # Installation guide (Português)
 # https://wiki.archlinux.org/title/Installation_guide_(Portugu%C3%AAs)
 
-# Funções auxiliares {{{
+# Funções auxiliares
 # Verifica a conectividade com a internet tentando pingar um servidor confiável
 is_connected() {
   _host='8.8.8.8'  # IP do DNS público do Google
@@ -12,93 +12,82 @@ is_connected() {
 
   ping -c "${_count}" -W "${_timeout}" "${_host}" > /dev/null 2>&1
 }
-# }}}
 
-# 1 - Pré-instalação {{{
+# 1 - Pré-instalação
 
-# 1.1 - Obter uma imagem de instalação {{{
-# }}}
+# 1.1 - Obter uma imagem de instalação
 
-# 1.2 - Verificar a assinatura {{{
-# }}}
+# 1.2 - Verificar a assinatura
 
-#1.3 - Preparar uma mídia de instalação {{{
-# }}}
+# 1.3 - Preparar uma mídia de instalação
 
-# 1.4 - Inicializar o ambiente live {{{
-# }}}
+# 1.4 - Inicializar o ambiente live
 
-# 1.5 - Definir o layout e fonte do teclado do console {{{
+# 1.5 - Definir o layout e fonte do teclado do console
 printf "Definindo o layout e fonte do teclado do console ...\n"
 loadkeys br-abnt2
 setfont ter-120n
-# }}}
 
-# 1.6 - Definir o idioma do ambiente live {{{
-# }}}
+# 1.6 - Definir o idioma do ambiente live
 
-# 1.7 - Verificar o modo de inicialização {{{
+# 1.7 - Verificar o modo de inicialização
 #cat /sys/firmware/efi/fw_platform_size
-# }}}
 
-# 1.8 - Conectar à internet {{{
-printf "Conectando à internet ...\n"
+# 1.8 - Conectar à internet
+printf "\nConectando à internet ...\n"
 
 # Verifica se foi possível conectar à internet
 # Se não foi possível conectar por uma interface ethernet, tenta através de uma
 # interface wireless
 if ! is_connected; then
-  printf "Não foi possível conectar à internet.\n"
+  printf "Não foi possível conectar à internet.\n\
+Conectando a uma rede sem fio ...\n\n"
 
   iwctl device list
 
-  printf "Escolha o dispositivo: "
+  printf "\nEscolha o dispositivo: "
   read -r _device
 
-  if iwctl device "${_device}" set-property Powered on; then
-    iwctl station "${_device}" scan
-    iwctl station "${_device}" get-networks
+  iwctl device "${_device}" set-property Powered off
+  iwctl device "${_device}" set-property Powered on
+  iwctl station "${_device}" scan
+  iwctl station "${_device}" get-networks
 
-    printf "Escolha a rede sem fio: "
-    read -r _ssid
+  printf "\nEscolha a rede sem fio: "
+  read -r _ssid
 
-    printf "Digite a senha: "
-    stty -echo
-    read -r _passphrase
-    stty echo
-    printf "\n"
+  printf "Digite a senha: "
+  stty -echo
+  read -r _passphrase
+  stty echo
+  printf "\n"
 
-    printf "Tentando conectar-se a rede %s ...\n" "${_ssid}"
-    iwctl --passphrase "${_passphrase}" station "${_device}" connect "${_ssid}"
-  else
-    printf "Não foi possível ligar o dispositivo %s\n" "${_device}"
-  fi
+  printf "Tentando conectar-se a rede %s ...\n" "${_ssid}"
+  iwctl --passphrase "${_passphrase}" station "${_device}" connect "${_ssid}"
 fi
 
 # Finaliza o script se não foi possível conectar à internet
 if ! is_connected; then
-  printf "Não foi possível conectar à internet.\nO script de instalação será \
-encerrado."
+  printf "\nNão foi possível conectar à internet.\n\
+O script de instalação será encerrado.\n"
   exit 1
 fi
-# }}}
 
-# 1.9 - Atualizar o relógio do sistema {{{
-printf "Atualizando o relógio do sistema ...\n"
+# 1.9 - Atualizar o relógio do sistema
+printf "\nAtualizando o relógio do sistema ...\n"
 timedatectl set-timezone America/Sao_Paulo
-# }}}
 
-# 1.10 - Partição dos discos {{{
-printf "Particionado os discos ...\n"
+# 1.10 - Partição dos discos
+printf "\nParticionado os discos ...\n"
 
-printf "O script utilizará o seguinte esquema de partições:\n\
+printf "\nO script utilizará o seguinte esquema de partições:\n\
 label: gpt\n\
 device: disco_escolhido\n\
 disco_escolhido1: EFI System       - tamanho: 36MiB\n\
-disco_escolhido2: Linux filesystem - tamanho: resto do disco\n"
+disco_escolhido2: Linux filesystem - tamanho: resto do disco\n\n"
 
 lsblk
-printf "Escolha o disco para instalação: "
+printf "\nEscolha o disco para instalação: "
 read -r _disk
 
 printf "O sistema será instalado no dispositivo %s\n\
@@ -107,7 +96,7 @@ Deseja continuar? Digite 's' para confirmar: " "${_disk}"
 read -r _continue
 
 if [ ! "${_continue}" = 's' ]; then
-  printf "O script de instalação será encerrado.\n"
+  printf "\nO script de instalação será encerrado.\n"
   exit 1
 fi
 
@@ -127,32 +116,26 @@ sector-size: 512
 /dev/${_disk}1 : start= , size=36M, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B
 /dev/${_disk}2 : start= , size= , type=0FC63DAF-8483-4772-8E79-3D69D8477DE4
 EOF
-# }}}
 
-# 1.11 - Formatar as partições {{{
-printf "Formatando as partições ...\n"
+# 1.11 - Formatar as partições
+printf "\nFormatando as partições ...\n"
 mkfs.fat -F 32 /dev/"${_disk}"1
 mkfs.ext4 -F /dev/"${_disk}"2
 #mkswap /dev/partição_swap
-# }}}
 
-# 1.12 - Montar os sistemas de arquivos {{{
-printf "Montando os sistemas de arquivos ...\n"
+# 1.12 - Montar os sistemas de arquivos
+printf "\nMontando os sistemas de arquivos ...\n"
 mount /dev/"${_disk}"2 /mnt
 mount --mkdir /dev/"${_disk}"1 /mnt/efi
 #swapon /dev/partição_swap
-#}}}
 
-# }}}
+# 2 - Instalação
 
-# 2 - Instalação {{{
-
-# 2.1 - Selecionar os espelhos {{{
+# 2.1 - Selecionar os espelhos
 # /etc/pacman.d/mirrorlist
-# }}}
 
-# 2.2 - Instalar os pacotes essenciais {{{
-printf "Instalando os pacotes essenciais ...\n"
+# 2.2 - Instalar os pacotes essenciais
+printf "\nInstalando os pacotes essenciais ...\n"
 
 # Verifica se o sistema possui um CPU AMD ou Intel para instalar o microcode
 [ "$(lscpu | grep --ignore-case --count 'amd')" -gt 0 ] \
@@ -174,36 +157,29 @@ printf "Instalando os pacotes essenciais ...\n"
 # neovim : Fork of Vim aiming to improve user experience, plugins, and GUIs
 # networkmanager : Network connection manager and user applications
 # ntfs-3g : NTFS filesystem driver and utilities
-# zsh : A very advanced and programmable command interpreter (shell) for UNIX
 pacstrap -K /mnt base base-devel ${cpu_microcode} dosfstools e2fsprogs \
   efibootmgr grub linux linux-firmware man-db man-pages neovim networkmanager \
-  ntfs-3g zsh
-# }}}
+  ntfs-3g
 
-# }}}
+# 3 - Configurar o sistema
+printf "\nConfigurando o sistema ...\n"
 
-# 3 - Configurar o sistema {{{
-printf "Configurando o sistema ...\n"
-
-# 3.1 - Fstab {{{
-printf "Fstab ...\n"
+# 3.1 - Fstab
+printf "\nFstab ...\n"
 genfstab -U /mnt >> /mnt/etc/fstab
-# }}}
 
-# 3.2 - Chroot {{{
-printf "Chroot ...\n"
+# 3.2 - Chroot
+printf "\nChroot ...\n"
 arch-chroot /mnt /bin/sh -c '
-# }}}
 
-# 3.3 - Horário {{{
-printf "Horário ...\n"
+# 3.3 - Horário
+printf "\nHorário ...\n"
 ln --symbolic --force /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 hwclock --systohc
 systemctl enable systemd-timesyncd.service
-# }}}
 
-# 3.4 - Localização {{{
-printf "Localização ...\n"
+# 3.4 - Localização
+printf "\nLocalização ...\n"
 # Remove o comentário de pt_BR.UTF-8 e gera os locales
 sed --in-place "s/^#\(pt_BR.UTF-8\)/\1/" /etc/locale.gen
 locale-gen
@@ -213,10 +189,9 @@ printf "LANG=pt_BR.UTF-8\n" > /etc/locale.conf
 
 # Armazena as definições do layout do teclado do console em vconsole.conf(5)
 printf "KEYMAP=br-abnt2\n" > /etc/vconsole.conf
-# }}}
 
-# 3.5 - Configuração de rede {{{
-printf "Configuração de rede ...\n"
+# 3.5 - Configuração de rede
+printf "\nConfiguração de rede ...\n"
 
 # Cria o arquivo hostname:
 printf "Digite seu hostname: "
@@ -238,19 +213,17 @@ EOF
 
 # Ativa o serviço do NetworkManager na inicialização do sistema
 systemctl enable NetworkManager.service
-# }}}
 
-# 3.6 - Initramfs {{{
-#printf "Initramfs ...\n"
+# 3.6 - Initramfs
+#printf "\nInitramfs ...\n"
 #mkinitcpio --allpresets
-# }}}
 
-# 3.7 - Senha do root {{{
-printf "Senha do root ...\n"
+# 3.7 - Senha do root
+printf "\nSenha do root ...\n"
 
 printf "Digite seu nome de usuário: "
 read -r _user
-useradd --create-home --groups wheel --shell /bin/zsh "${_user}"
+useradd --create-home --groups wheel "${_user}"
 passwd "${_user}"
 
 # Permite que usuários do grupo wheel executem qualquer comando
@@ -259,23 +232,18 @@ sed --in-place "s/^# *\(%wheel ALL=(ALL:ALL) ALL\)/\1/" /etc/sudoers
 
 # Desabilita o login do usuário root
 passwd --lock root
-# }}}
 
-# 3.8 - Gerenciador de boot {{{
-printf "Gerenciador de boot ...\n"
+# 3.8 - Gerenciador de boot
+printf "\nGerenciador de boot ...\n"
 grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
 grub-mkconfig --output=/boot/grub/grub.cfg
-# }}}
 
-# }}}
-
-# 4 - Reiniciar {{{
+# 4 - Reiniciar
 exit
 '
-printf "Instalação finalizada.\n\
+printf "\nInstalação finalizada.\n\
 Pressione ENTER para reiniciar.\n"
 read -r dummy
 
 umount -R /mnt
 reboot
-# }}}
