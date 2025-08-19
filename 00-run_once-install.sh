7343c98a2664d663fb14ebeacd3049d5a2e48887
maozinha23 <20055270+maozinha23@users.noreply.github.com>
2025-08-19 20:13:01 -0300

    Copia o script pós-instalação para a raiz do novo sistema

--- 00-run_once-install.sh
+++ 00-run_once-install.sh
@@ -2,8 +2,9 @@
 
 # Installation guide (Português)
 # https://wiki.archlinux.org/title/Installation_guide_(Portugu%C3%AAs)
-
+#-------------------------------------------------------------------------------
 # Funções auxiliares
+#-------------------------------------------------------------------------------
 # Verifica a conectividade com a internet tentando pingar um servidor confiável
 is_connected() {
   _host='8.8.8.8'  # IP do DNS público do Google
@@ -12,27 +13,28 @@ is_connected() {
 
   ping -c "${_count}" -W "${_timeout}" "${_host}" > /dev/null 2>&1
 }
-
+#-------------------------------------------------------------------------------
 # 1 - Pré-instalação
-
+#-------------------------------------------------------------------------------
 # 1.1 - Obter uma imagem de instalação
-
+#-------------------------------------------------------------------------------
 # 1.2 - Verificar a assinatura
-
+#-------------------------------------------------------------------------------
 # 1.3 - Preparar uma mídia de instalação
-
+#-------------------------------------------------------------------------------
 # 1.4 - Inicializar o ambiente live
-
+#-------------------------------------------------------------------------------
 # 1.5 - Definir o layout e fonte do teclado do console
+#-------------------------------------------------------------------------------
 printf "Definindo o layout e fonte do teclado do console ...\n"
 loadkeys br-abnt2
 setfont ter-120n
-
+#-------------------------------------------------------------------------------
 # 1.6 - Definir o idioma do ambiente live
-
+#-------------------------------------------------------------------------------
 # 1.7 - Verificar o modo de inicialização
 #cat /sys/firmware/efi/fw_platform_size
-
+#-------------------------------------------------------------------------------
 # 1.8 - Conectar à internet
 printf "\nConectando à internet ...\n"
 
@@ -72,14 +74,15 @@ if ! is_connected; then
 O script de instalação será encerrado.\n"
   exit 1
 fi
-
+#-------------------------------------------------------------------------------
 # 1.9 - Atualizar o relógio do sistema
+#-------------------------------------------------------------------------------
 printf "\nAtualizando o relógio do sistema ...\n"
 timedatectl set-timezone America/Sao_Paulo
-
+#-------------------------------------------------------------------------------
 # 1.10 - Partição dos discos
+#-------------------------------------------------------------------------------
 printf "\nParticionado os discos ...\n"
-
 printf "\nO script utilizará o seguinte esquema de partições:\n\
 label: gpt\n\
 device: disco_escolhido\n\
@@ -116,25 +119,29 @@ sector-size: 512
 /dev/${_disk}1 : start= , size=36M, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B
 /dev/${_disk}2 : start= , size= , type=0FC63DAF-8483-4772-8E79-3D69D8477DE4
 EOF
-
+#-------------------------------------------------------------------------------
 # 1.11 - Formatar as partições
+#-------------------------------------------------------------------------------
 printf "\nFormatando as partições ...\n"
 mkfs.fat -F 32 /dev/"${_disk}"1
 mkfs.ext4 -F /dev/"${_disk}"2
 #mkswap /dev/partição_swap
-
+#-------------------------------------------------------------------------------
 # 1.12 - Montar os sistemas de arquivos
+#-------------------------------------------------------------------------------
 printf "\nMontando os sistemas de arquivos ...\n"
 mount /dev/"${_disk}"2 /mnt
 mount --mkdir /dev/"${_disk}"1 /mnt/efi
 #swapon /dev/partição_swap
-
+#-------------------------------------------------------------------------------
 # 2 - Instalação
-
+#-------------------------------------------------------------------------------
 # 2.1 - Selecionar os espelhos
+#-------------------------------------------------------------------------------
 # /etc/pacman.d/mirrorlist
-
+#-------------------------------------------------------------------------------
 # 2.2 - Instalar os pacotes essenciais
+#-------------------------------------------------------------------------------
 printf "\nInstalando os pacotes essenciais ...\n"
 
 # Verifica se o sistema possui um CPU AMD ou Intel para instalar o microcode
@@ -160,25 +167,30 @@ printf "\nInstalando os pacotes essenciais ...\n"
 pacstrap -K /mnt base base-devel ${cpu_microcode} dosfstools e2fsprogs \
   efibootmgr grub linux linux-firmware man-db man-pages neovim networkmanager \
   ntfs-3g
-
+#-------------------------------------------------------------------------------
 # 3 - Configurar o sistema
+#-------------------------------------------------------------------------------
 printf "\nConfigurando o sistema ...\n"
-
+#-------------------------------------------------------------------------------
 # 3.1 - Fstab
+#-------------------------------------------------------------------------------
 printf "\nFstab ...\n"
 genfstab -U /mnt >> /mnt/etc/fstab
-
+#-------------------------------------------------------------------------------
 # 3.2 - Chroot
+#-------------------------------------------------------------------------------
 printf "\nChroot ...\n"
 arch-chroot /mnt /bin/sh -c '
-
+#-------------------------------------------------------------------------------
 # 3.3 - Horário
+#-------------------------------------------------------------------------------
 printf "\nHorário ...\n"
 ln --symbolic --force /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 hwclock --systohc
 systemctl enable systemd-timesyncd.service
-
+#-------------------------------------------------------------------------------
 # 3.4 - Localização
+#-------------------------------------------------------------------------------
 printf "\nLocalização ...\n"
 # Remove o comentário de pt_BR.UTF-8 e gera os locales
 sed --in-place "s/^#\(pt_BR.UTF-8\)/\1/" /etc/locale.gen
@@ -189,8 +201,9 @@ printf "LANG=pt_BR.UTF-8\n" > /etc/locale.conf
 
 # Armazena as definições do layout do teclado do console em vconsole.conf(5)
 printf "KEYMAP=br-abnt2\n" > /etc/vconsole.conf
-
+#-------------------------------------------------------------------------------
 # 3.5 - Configuração de rede
+#-------------------------------------------------------------------------------
 printf "\nConfiguração de rede ...\n"
 
 # Cria o arquivo hostname:
@@ -213,14 +226,15 @@ EOF
 
 # Ativa o serviço do NetworkManager na inicialização do sistema
 systemctl enable NetworkManager.service
-
+#-------------------------------------------------------------------------------
 # 3.6 - Initramfs
+#-------------------------------------------------------------------------------
 #printf "\nInitramfs ...\n"
 #mkinitcpio --allpresets
-
+#-------------------------------------------------------------------------------
 # 3.7 - Senha do root
+#-------------------------------------------------------------------------------
 printf "\nSenha do root ...\n"
-
 printf "Digite seu nome de usuário: "
 read -r _user
 useradd --create-home --groups wheel "${_user}"
@@ -232,18 +246,21 @@ sed --in-place "s/^# *\(%wheel ALL=(ALL:ALL) ALL\)/\1/" /etc/sudoers
 
 # Desabilita o login do usuário root
 passwd --lock root
-
+#-------------------------------------------------------------------------------
 # 3.8 - Gerenciador de boot
+#-------------------------------------------------------------------------------
 printf "\nGerenciador de boot ...\n"
 grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
 grub-mkconfig --output=/boot/grub/grub.cfg
-
+#-------------------------------------------------------------------------------
 # 4 - Reiniciar
+#-------------------------------------------------------------------------------
 exit
 '
 printf "\nInstalação finalizada.\n\
 Pressione ENTER para reiniciar.\n"
-read -r dummy
+read -r _
 
+cp arch-install/01-run_once-post_install.sh /mnt/
 umount -R /mnt
 reboot
